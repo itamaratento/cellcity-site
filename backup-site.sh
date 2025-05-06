6ed0e6d37231335fa54d80b6025493560929570a
Itamar Atento <itamaratento@gmail.com>
2025-05-06 13:51:39 -0300

    Atualização automática

--- backup-site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DATAHORA=$(date +"%Y-%m-%d_%H-%M-%S")
-mkdir -p /c/projetos/backups-cellcity/$DATAHORA
-cp -r * /c/projetos/backups-cellcity/$DATAHORA
-echo "Backup feito com sucesso em /c/projetos/backups-cellcity/$DATAHORA"
---
+++ backupe-one-drive.bat
@@ -0,0 +1,7 @@
+
+@echo off
+echo Copiando arquivos do projeto para o OneDrive...
+xcopy "C:\Users\ITAMAR\cellcity-site\*" "C:\Users\ITAMAR\OneDrive\cellcity-site\" /E /H /Y /C
+echo Concluído!
+pause
+
